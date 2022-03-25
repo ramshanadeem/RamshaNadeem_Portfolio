c23fc72a56b20fdcb18088308c91b44c24ec2e13
ramshanadeem <rimshanadeem64@gmail.com>
2022-03-25 10:47:37 +0500

    portfolio

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Navbar from "./navbar";
+import IntroSection from "./introSection";
+import AboutSection from "./aboutSection";
+import ContactSection from "./contactSection";
+export { Navbar, IntroSection, AboutSection, ContactSection };
---
+++ src/components/introSection/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function IntroSection() {
+  return <div>index</div>;
+}
+
+export default IntroSection;
